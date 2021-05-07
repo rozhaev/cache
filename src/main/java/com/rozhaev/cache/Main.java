b1fb156c98a2eb8090b1bfbcd611d44adb987ffb
Dmitry Rozhaev <dmitry.rozhaev@gmail.com>
2021-05-07 14:05:58 +0300

    updated main

--- src/main/java/com/rozhaev/cache/Main.java
+++ src/main/java/com/rozhaev/cache/Main.java
@@ -2,6 +2,14 @@ package com.rozhaev.cache;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        var cache = new FutureCache<String, String>();
+        try {
+            cache.setValue("A", "B1");
+            cache.setValue("A", "B1");
+            System.out.println(cache.getValue("A", String::new));
+            System.out.println(cache.getValue("B", String::new));
+        } catch (Exception e) {
+            System.out.println("=)");
+        }
     }
 }
